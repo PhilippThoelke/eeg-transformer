784a662c7c2b8fbaae08e7896e99bae6f05b404b
Philipp Thölke <philipp.thoelke@posteo.de>
2023-01-03 16:04:42 -0500

    Reduce augmentation strength

--- eegt/augmentation.py
+++ eegt/augmentation.py
@@ -2,17 +2,17 @@ import torch
 from torchaudio.functional import resample as _resample
 
 
-def gaussian_noise(x, ch_pos, mask, noise_scale=0.1):
+def gaussian_noise(x, ch_pos, mask, noise_scale=0.05):
     """Add Gaussian noise to the signal"""
     return x + torch.randn_like(x) * x.std() * noise_scale, ch_pos, mask
 
 
-def gaussian_noise_channel_pos(x, ch_pos, mask, noise_scale=0.1):
+def gaussian_noise_channel_pos(x, ch_pos, mask, noise_scale=0.05):
     """Add Gaussian noise to the channel positions"""
     return x, ch_pos + torch.randn_like(ch_pos) * ch_pos.std() * noise_scale, mask
 
 
-def gaussian_noise_freq(x, ch_pos, mask, noise_scale=0.12):
+def gaussian_noise_freq(x, ch_pos, mask, noise_scale=0.09):
     """Add Gaussian noise to the Fourier transformed signal and
     transform back into the temporal domain"""
     ft = torch.fft.fft(x, dim=1)
@@ -22,7 +22,7 @@ def gaussian_noise_freq(x, ch_pos, mask, noise_scale=0.12):
     return torch.fft.ifft(ft, dim=1).real, ch_pos, mask
 
 
-def rescale(x, ch_pos, mask, max_strength=0.3):
+def rescale(x, ch_pos, mask, max_strength=0.2):
     """Scale the signal by a random scalar around 1"""
     scalers = torch.rand(x.size(0), 1, x.size(2), device=x.device)
     scalers = scalers * 2 * max_strength + 1 - max_strength
@@ -30,7 +30,7 @@ def rescale(x, ch_pos, mask, max_strength=0.3):
     return (x - mean) * scalers + mean, ch_pos, mask
 
 
-def rescale_channel_pos(x, ch_pos, mask, max_strength=0.1):
+def rescale_channel_pos(x, ch_pos, mask, max_strength=0.05):
     """Scale channel positions by a random scalar around 1"""
     scalers = torch.rand(ch_pos.size(0), 1, 1, device=ch_pos.device)
     scalers = scalers * 2 * max_strength + 1 - max_strength
@@ -38,21 +38,21 @@ def rescale_channel_pos(x, ch_pos, mask, max_strength=0.1):
     return x, (ch_pos - mean) * scalers + mean, mask
 
 
-def random_mean(x, ch_pos, mask, max_strength=0.15):
+def random_mean(x, ch_pos, mask, max_strength=0.075):
     """Add a random mean to the signal"""
     means = torch.rand(x.size(0), 1, x.size(2), device=x.device)
     means = means * 2 * max_strength - max_strength
     return x + means, ch_pos, mask
 
 
-def random_mean_channel_pos(x, ch_pos, mask, max_strength=0.1):
+def random_mean_channel_pos(x, ch_pos, mask, max_strength=0.05):
     """Add a random mean to the channel positions"""
     means = torch.rand(ch_pos.size(0), 1, 1, device=x.device)
     means = means * 2 * max_strength - max_strength
     return x, ch_pos + means, mask
 
 
-def shift_samples(x, ch_pos, mask, max_n=16):
+def shift_samples(x, ch_pos, mask, max_n=8):
     """Randomly shift the signal forwards of backwards in time"""
     shifts = torch.randint(1, max_n + 1, (x.size(0),), device=x.device)
     shifts[torch.rand(shifts.shape, device=x.device) < 0.5] *= -1
@@ -68,7 +68,7 @@ def flip_sign(x, ch_pos, mask, prob=0.5):
     return x, ch_pos, mask
 
 
-def resample(x, ch_pos, mask, max_change_pct=40):
+def resample(x, ch_pos, mask, max_change_pct=20):
     """Downsample the signal to a lower frequency,
     follwed by upsampling to the original sampling rate"""
     x = x.permute(0, 2, 1).contiguous()
@@ -79,7 +79,7 @@ def resample(x, ch_pos, mask, max_change_pct=40):
     return x.permute(0, 2, 1), ch_pos, mask
 
 
-def temporal_dropout(x, ch_pos, mask, max_samples=64, prob=0.2):
+def temporal_dropout(x, ch_pos, mask, max_samples=32, prob=0.15):
     """Set a random section of a random subset of channels to 0"""
     mask = torch.rand(x.size(0), x.size(2), device=x.device) < prob
     sizes = torch.randint(1, max_samples + 1, (mask.sum(),), device=x.device)
@@ -89,7 +89,7 @@ def temporal_dropout(x, ch_pos, mask, max_samples=64, prob=0.2):
     return x, ch_pos, mask
 
 
-def channel_dropout(x, ch_pos, mask, min_rate=0.1, max_rate=0.7):
+def channel_dropout(x, ch_pos, mask, min_rate=0.1, max_rate=0.5):
     """Randomly mask out channels with a probability between min_rate and max_rate"""
     if mask is None:
         mask = torch.ones(x.size(0), x.size(2), dtype=torch.bool, device=x.device)
@@ -104,7 +104,7 @@ def channel_dropout(x, ch_pos, mask, min_rate=0.1, max_rate=0.7):
     return x, ch_pos, mask
 
 
-def randomize_phase(x, ch_pos, mask, strength=0.5):
+def randomize_phase(x, ch_pos, mask, strength=0.3):
     """Randomize the Fourier phase of the EEG"""
     # transform the data into frequency domain
     ft = torch.fft.fft(x, dim=1)
