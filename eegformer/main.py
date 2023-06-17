cded71de8bba5263229e0d8319a96174487d003f
PhilippThoelke <philipp.thoelke@posteo.de>
2023-06-17 14:37:16 -0700

    Format

--- eegformer/main.py
+++ eegformer/main.py
@@ -7,5 +7,6 @@ from eegformer.model import Transformer
 def main():
     cli = LightningCLI(model_class=Transformer, datamodule_class=PhysionetMotorImagery, save_config_callback=False)
 
+
 if __name__ == "__main__":
     main()
