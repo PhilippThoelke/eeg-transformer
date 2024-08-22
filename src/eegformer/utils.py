fa0031fe657fe509f1e178291fa67086380f0dd1
PhilippThoelke <philipp.thoelke@posteo.de>
2024-08-22 18:06:38 +0200

    Fix debug plot

--- src/eegformer/utils.py
+++ src/eegformer/utils.py
@@ -17,18 +17,18 @@ def plot_gradients(module):
 
     layers, ave_grads, max_grads, ave_weights, max_weights = [], [], [], [], []
     for n, p in module.named_parameters():
-        if (p.requires_grad) and ("bias" not in n):
+        if (p.requires_grad) and ("bias" not in n) and ("norm" not in n):
             layers.append(n)
             ave_grads.append(p.grad.abs().mean().item())
             max_grads.append(p.grad.abs().max().item())
             ave_weights.append(p.detach().abs().mean().item())
             max_weights.append(p.detach().abs().max().item())
     ave_grads, max_grads = np.array(ave_grads), np.array(max_grads)
-    ave_grads[np.isinf(ave_grads)] = -1
-    max_grads[np.isinf(max_grads)] = -1
+    ave_grads[np.isinf(ave_grads) | np.isnan(ave_grads)] = -1
+    max_grads[np.isinf(max_grads) | np.isnan(max_grads)] = -1
     ave_weights, max_weights = np.array(ave_weights), np.array(max_weights)
-    ave_weights[np.isinf(ave_weights)] = -1
-    max_weights[np.isinf(max_weights)] = -1
+    ave_weights[np.isinf(ave_weights) | np.isnan(ave_weights)] = -1
+    max_weights[np.isinf(max_weights) | np.isnan(max_weights)] = -1
 
     plt.subplot(211)
     plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.5, label="max-gradient")
