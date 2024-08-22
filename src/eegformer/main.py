c53921adfcaee4a58121f442ba3401fa9362f763
PhilippThoelke <philipp.thoelke@posteo.de>
2024-08-22 19:12:52 +0200

    Fix lightning version issue

--- src/eegformer/main.py
+++ src/eegformer/main.py
@@ -1,6 +1,5 @@
 from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
 from lightning.pytorch.cli import LightningCLI
-from lightning.pytorch.loggers import TensorBoardLogger
 
 from eegformer.data import DataModule
 from eegformer.module import LightningModule
@@ -17,6 +16,6 @@ if __name__ == "__main__":
                 LearningRateMonitor(logging_interval="step"),
                 ModelCheckpoint(monitor="loss/val", filename="{epoch}-{step}-{val_loss:.2f}", save_last=True),
             ],
-            logger=TensorBoardLogger("", default_hp_metric=False),
+            logger=dict(class_path="TensorBoardLogger", init_args=dict(save_dir="logs", default_hp_metric=False)),
         ),
     )
